ca4405af374a876d37f85094ae1b74a1590af641
Taylor Feinstein <Taylor Feinstein>
2017-12-03 17:02:02 -0600

    config file

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(methodOverride("_method"));
 // require our API routes
 require("./routes/api-routes.js")(app);
 // Server will Listen on this port
-var server = app.listen(process.env.PORT || 5050, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     rowdy.print();
 });
 
